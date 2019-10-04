6bffbf67d5ff45b38fb6e3d8d512ad2105faead7
Brad Nissen <bradanissen@gmail.com>
2019-10-03 18:14:25 -0700

    better formatting

--- var.tf
+++ var.tf
@@ -5,26 +5,25 @@ variable "aad_tenant_id" {
 
 variable "user_object_id" {
   description = "The object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault."
-  default = "192a13e7-fa31-4ee1-9e1a-f7c46c9a82b6"
+  default     = "192a13e7-fa31-4ee1-9e1a-f7c46c9a82b6"
 }
 
 variable "resource_group_name" {
   description = "The name of the resource group being created."
-  default = "amido-uks-cb170-rg-bot-dev"
+  default     = "amido-uks-cb170-rg-bot-dev"
 }
 
 variable "location" {
-  description ="The location to deploy the resource."
-  default ="UK South"
+  description = "The location to deploy the resource."
+  default     = "UK South"
 }
 
 variable "key_vault_name" {
-  description ="The name of the keyvault being created."
-  default = "amido-uks-cb170-kv-dev"
+  description = "The name of the keyvault being created."
+  default     = "amido-uks-cb170-kv-dev"
 }
-
 
 variable "storage_acct_name" {
   description = "The name of the storage account being created."
-  default = "amidoukscb170"
+  default     = "amidoukscb170"
 }
